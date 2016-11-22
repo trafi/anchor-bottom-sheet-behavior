b8ac88bad33e47a61de12683c1d5e1600e92dfaa
Justas Medeisis <justas@trafi.com>
2016-11-22 10:03:55 +0200

    Add sample layout.

--- app/src/main/java/com/trafi/anchorbottomsheetbehavior/MainActivity.java
+++ app/src/main/java/com/trafi/anchorbottomsheetbehavior/MainActivity.java
@@ -1,13 +1,33 @@
 package com.trafi.anchorbottomsheetbehavior;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Random r = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final View background = findViewById(R.id.coordinator_layout);
+        View.OnClickListener onClickChangeBackgroundColor = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                background.setBackgroundColor(
+                        Color.argb(255, r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+            }
+        };
+
+        background.setOnClickListener(onClickChangeBackgroundColor);
+        findViewById(R.id.button_the_first).setOnClickListener(onClickChangeBackgroundColor);
+        findViewById(R.id.button_the_second).setOnClickListener(onClickChangeBackgroundColor);
+        findViewById(R.id.button_the_third).setOnClickListener(onClickChangeBackgroundColor);
     }
 }
