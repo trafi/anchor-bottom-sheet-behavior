2c8a96e5491ed1144ca7c91a98ecd50063cf0a99
Eligijus Starinskas <eligijus@trafi.com>
2017-08-01 13:49:08 +0300

    Implement view pager support

---
+++ library/src/main/java/com/trafi/anchorbottomsheetbehavior/BottomSheetUtils.java
@@ -0,0 +1,51 @@
+package com.trafi.anchorbottomsheetbehavior;
+
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+public class BottomSheetUtils {
+
+    public static void setupViewPager(ViewPager viewPager) {
+        final View bottomSheetParent = findBottomSheetParent(viewPager);
+        if (bottomSheetParent != null) {
+            viewPager.addOnPageChangeListener(new BottomSheetViewPagerListener(viewPager, bottomSheetParent));
+        }
+    }
+
+    private static class BottomSheetViewPagerListener extends ViewPager.SimpleOnPageChangeListener {
+        private final ViewPager viewPager;
+        private final AnchorBottomSheetBehavior<View> behavior;
+
+        public BottomSheetViewPagerListener(ViewPager viewPager, View bottomSheetParent) {
+            this.viewPager = viewPager;
+            this.behavior = AnchorBottomSheetBehavior.from(bottomSheetParent);
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            viewPager.post(new Runnable() {
+                @Override
+                public void run() {
+                    behavior.invalidateScrollingChild();
+                }
+            });
+        }
+    }
+
+    private static View findBottomSheetParent(final View view) {
+        View current = view;
+        while (current != null) {
+            final ViewGroup.LayoutParams params = current.getLayoutParams();
+            if (params instanceof CoordinatorLayout.LayoutParams && ((CoordinatorLayout.LayoutParams) params).getBehavior() instanceof AnchorBottomSheetBehavior) {
+                return current;
+            }
+            final ViewParent parent = current.getParent();
+            current = parent == null || !(parent instanceof View) ? null : (View) parent;
+        }
+        return null;
+    }
+
+}
