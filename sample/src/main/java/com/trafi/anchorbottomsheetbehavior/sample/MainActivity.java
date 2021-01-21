0966eebdf8c7b9f75e4c1227c8a36abd778ad57d
Eligijus Starinskas <eligijus@trafi.com>
2021-01-21 17:43:28 +0200

    Add viewpager2 example to sample project

--- sample/src/main/java/com/trafi/anchorbottomsheetbehavior/sample/MainActivity.java
+++ sample/src/main/java/com/trafi/anchorbottomsheetbehavior/sample/MainActivity.java
@@ -2,9 +2,18 @@ package com.trafi.anchorbottomsheetbehavior.sample;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.trafi.anchorbottomsheetbehavior.BottomSheetUtils;
+
 import java.util.Random;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final View background = findViewById(R.id.coordinator_layout);
-        View.OnClickListener onClickChangeBackgroundColor = new View.OnClickListener() {
+        final View.OnClickListener onClickChangeBackgroundColor = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 background.setBackgroundColor(
@@ -24,9 +33,34 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+        ViewPager2 pager = findViewById(R.id.pager);
+        BottomSheetUtils.setupViewPager2(pager);
+
+        pager.setAdapter(new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return new RecyclerView.ViewHolder(LayoutInflater.from(getApplicationContext()).inflate(R.layout.viewpager_page_layout, parent, false)) {
+                    @Override
+                    public String toString() {
+                        return super.toString();
+                    }
+                };
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+                holder.itemView.findViewById(R.id.button_the_first).setOnClickListener(onClickChangeBackgroundColor);
+                holder.itemView.findViewById(R.id.button_the_second).setOnClickListener(onClickChangeBackgroundColor);
+                holder.itemView.findViewById(R.id.button_the_third).setOnClickListener(onClickChangeBackgroundColor);
+            }
+
+            @Override
+            public int getItemCount() {
+                return 2;
+            }
+        });
+
         background.setOnClickListener(onClickChangeBackgroundColor);
-        findViewById(R.id.button_the_first).setOnClickListener(onClickChangeBackgroundColor);
-        findViewById(R.id.button_the_second).setOnClickListener(onClickChangeBackgroundColor);
-        findViewById(R.id.button_the_third).setOnClickListener(onClickChangeBackgroundColor);
     }
 }
